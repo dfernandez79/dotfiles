761ab962337caa42c86c90455daf52ed9b94c127
dfernandez79 <diegof79@gmail.com>
2022-09-27 10:26:40 -0300

    Update VSCode plugin list

--- install-vscode-extensions.sh
+++ install-vscode-extensions.sh
@@ -2,19 +2,16 @@
 
 CODE="/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code"
 
-"$CODE" --install-extension alefragnani.Bookmarks
 "$CODE" --install-extension bierner.github-markdown-preview
 "$CODE" --install-extension DavidAnson.vscode-markdownlint
 "$CODE" --install-extension dbaeumer.vscode-eslint
 "$CODE" --install-extension eamodio.gitlens
-"$CODE" --install-extension EditorConfig.EditorConfig
 "$CODE" --install-extension esbenp.prettier-vscode
 "$CODE" --install-extension JounQin.vscode-mdx
 "$CODE" --install-extension styled-components.vscode-styled-components
 "$CODE" --install-extension ms-python.python
 "$CODE" --install-extension ms-python.vscode-pylance
 "$CODE" --install-extension ms-toolsai.jupyter
-"$CODE" --install-extension ms-vsliveshare.vsliveshare
 "$CODE" --install-extension pflannery.vscode-versionlens
 "$CODE" --install-extension streetsidesoftware.code-spell-checker
 "$CODE" --install-extension streetsidesoftware.code-spell-checker-spanish
@@ -22,3 +19,4 @@ CODE="/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code"
 "$CODE" --install-extension stkb.rewrap
 "$CODE" --install-extension vsls-contrib.codetour
 "$CODE" --install-extension gruntfuggly.todo-tree
+"$CODE" --install-extension Orta.vscode-jest

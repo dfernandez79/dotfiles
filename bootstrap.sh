819c7246cb88d5a3cc15d8dab3ce651b5399ea71
Diego <diego@Diegos-Virtual-Machine.local>
2023-10-15 17:05:43 -0700

    env

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,89 @@
 #!/usr/bin/env bash
-brew bundle --file=- <<EOF
+
+# Make sure to cancel the whole script when Ctrl-C is pressed
+trap "exit" INT
+
+# Install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+# mas "Gifski", id: 1351639930
+# mas "Hidden Bar", id: 1452453066
+# mas "Keynote", id: 409183694
+# mas "MindNode", id: 1289197285
+# mas "Moom", id: 419330170
+# mas "Numbers", id: 409203825
+# mas "Pages", id: 409201541
+# mas "Pixelmator Pro", id: 1289583905
+# mas "Streaks", id: 963034692
+# mas "WhatsApp", id: 1147396723
+# mas "Xcode", id: 497799835
+
+
+# --- Brewfile start ---
+brew bundle --no-lock --file=- <<EOF
+brew "antigen"
+brew "chezmoi"
+brew "fnm"
+brew "gh"
+brew "git"
+brew "pinentry"
+brew "gnupg"
+brew "graphviz"
+brew "hyperfine"
+brew "jq"
+brew "lsd"
+brew "mas"
+brew "pinentry-mac"
+brew "ripgrep"
+brew "starship"
+brew "zsh"
+
+cask "appcleaner"
+cask "font-fira-code-nerd-font"
+cask "gitup"
+cask "handbrake"
+cask "iterm2"
+cask "keycastr"
+cask "obsidian"
+cask "sf-symbols"
+cask "utm"
+cask "visual-studio-code"
+cask "vlc"
+
+vscode "bierner.github-markdown-preview"
+vscode "bierner.markdown-checkbox"
+vscode "bierner.markdown-emoji"
+vscode "bierner.markdown-footnotes"
+vscode "bierner.markdown-mermaid"
+vscode "bierner.markdown-preview-github-styles"
+vscode "bierner.markdown-yaml-preamble"
+vscode "DavidAnson.vscode-markdownlint"
+vscode "dbaeumer.vscode-eslint"
+vscode "eamodio.gitlens"
+vscode "EditorConfig.EditorConfig"
+vscode "esbenp.prettier-vscode"
+vscode "GitHub.vscode-pull-request-github"
+vscode "Gruntfuggly.todo-tree"
+vscode "humao.rest-client"
+vscode "ms-vsliveshare.vsliveshare"
+vscode "Orta.vscode-jest"
+vscode "pflannery.vscode-versionlens"
+vscode "stkb.rewrap"
+vscode "streetsidesoftware.code-spell-checker"
+vscode "streetsidesoftware.code-spell-checker-spanish"
+vscode "styled-components.vscode-styled-components"
+vscode "unifiedjs.vscode-mdx"
+vscode "vscode-icons-team.vscode-icons"
+EOF
+# --- Brewfile end ---
+
+# Use the latest zsh version from Homebrew
+if ! fgrep -q "${HOMEBREW_PREFIX}/zsh" /etc/shells; then
+    echo "Adding ${HOMEBREW_PREFIX}/zsh to /etc/shells"
+    echo "${HOMEBREW_PREFIX}/zsh" | sudo tee -a /etc/shells;
+    chsh -s "${HOMEBREW_PREFIX}/zsh";
+fi;
+
+chezmoi init -S .
 
--- get-iterm-config.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-DOTFILES_DIR="$(cd "$(dirname "${BASH_SOURCE}")"; pwd)"
-BACKUP_DIR="${DOTFILES_DIR}/backup"
-
-cp "$HOME/Library/Preferences/com.googlecode.iterm2.plist" $DOTFILES_DIR
-plutil -convert xml1 com.googlecode.iterm2.plist
